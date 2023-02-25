fc0e050fd2b47c96cba69ce4e20dd3ffd9d7e6d9
Philypy Gomes <phcg.dev@gmail.com>
2023-02-24 22:24:19 -0300

    anatomia java inicial

---
+++ src/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    
+    public static void main(String[] args) {
+
+        System.out.println ("Olá turma, sejam bem vindos");
+        Systen.out.println ("Elaborando a lista de alunos")
+        
+        
+    }
+    
+}
